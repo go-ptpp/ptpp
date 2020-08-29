165e410d39284e15ff0ce696847078982f465ead
Ali AslRousta <aslrousta@gmail.com>
2020-08-29 21:40:58 +0430

    protect against mutual access

--- semantic.go
+++ semantic.go
@@ -1,13 +1,19 @@
 package ptpp
 
+import "sync"
+
 // DefaultSemanticMatcher is a SemanticMatcher which uses a simple lookup table
 // to find the best suggestion.
 type DefaultSemanticMatcher struct {
 	contexts map[string]wordList
+	mutex    sync.RWMutex
 }
 
 // Match finds the best suggestion based on the context.
 func (sm *DefaultSemanticMatcher) Match(context string, suggestions []string) (string, bool) {
+	sm.mutex.RLock()
+	defer sm.mutex.RUnlock()
+
 	if sm.contexts == nil {
 		return suggestions[0], false
 	}
@@ -25,6 +31,9 @@ func (sm *DefaultSemanticMatcher) Match(context string, suggestions []string) (s
 
 // Train trains the semantic models with a word and its context.
 func (sm *DefaultSemanticMatcher) Train(context, word string) {
+	sm.mutex.Lock()
+	defer sm.mutex.Unlock()
+
 	if sm.contexts == nil {
 		sm.contexts = make(map[string]wordList)
 	}
--- spell.go
+++ spell.go
@@ -1,6 +1,9 @@
 package ptpp
 
-import "unicode/utf8"
+import (
+	"sync"
+	"unicode/utf8"
+)
 
 type wordList map[string]bool
 
@@ -17,10 +20,14 @@ func (w wordList) Has(word string) bool {
 // suggestions for a misspelled word.
 type DefaultSpellChecker struct {
 	lexicon map[int]wordList
+	mutex   sync.RWMutex
 }
 
 // Check finds correct spell suggestions for a word.
 func (sc *DefaultSpellChecker) Check(word string) []string {
+	sc.mutex.RLock()
+	defer sc.mutex.RUnlock()
+
 	length := utf8.RuneCountInString(word)
 	if sc.lexicon == nil || length < 2 {
 		return []string{word}
@@ -67,6 +74,9 @@ func (sc *DefaultSpellChecker) Check(word string) []string {
 
 // Train trains the suggestion model with a list of words.
 func (sc *DefaultSpellChecker) Train(words []string) {
+	sc.mutex.Lock()
+	defer sc.mutex.Unlock()
+
 	for _, w := range words {
 		sc.trainWord(w)
 	}
